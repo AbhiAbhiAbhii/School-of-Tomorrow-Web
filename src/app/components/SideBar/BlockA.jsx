b8d7f2ce6619402dce45f8181bb8ea96c3980eb3
AbhiAbhiAbhii <abhilash@gradical.xyz>
2024-07-17 14:35:27 +0530

    Register now link updates

--- src/app/components/SideBar/BlockA.jsx
+++ src/app/components/SideBar/BlockA.jsx
@@ -6,7 +6,7 @@ export default function BlockA({ className }) {
   return (
     <Link
       className={`flex items-center justify-center gap-2 py-2 bg-white rounded-sm ${className}`}
-      href='/'
+      href='https://docs.google.com/forms/d/e/1FAIpQLSdfhQ-XhnXIebqcIifyQqZ-kcN7KgEgdpE-VRSKKJoopzpP5g/viewform'
     >
       <p className="font-PPEdNewRegular text-purpleB text-[30px]">
         Register Now
