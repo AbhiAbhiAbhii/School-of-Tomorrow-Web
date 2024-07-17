cbd1bb687c7eb39973f191b67d1c7ac5f6d6f85e
AbhiAbhiAbhii <abhilash@gradical.xyz>
2024-07-17 16:15:15 +0530

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,11 +4,10 @@ import HeroBannerText from "./components/HeroBannerText";
 import HexShowcase from "./components/HexShowcase";
 import BeliefShowcase from "./components/BeliefShowcase";
 import SpeakerShowcase from "./components/SpeakerShowcase";
-import Reveal from "./components/Reveal";
 import WorkShopShowcase from "./components/WorkShopShowcase";
 import Agenda from "./components/Agenda";
 import LogoShowcase from "./components/LogoShowcase";
-import { Collaborators, Organizers, OrganizerSVG } from "../../logoData";
+import { Collaborators, OrganizerSVG } from "../../logoData";
 import TicketShowcase from "./components/TicketShowcase";
 import Link from "next/link";
 
@@ -17,7 +16,6 @@ export default function Home() {
 
   return (
     <section className="flex justify-between gap-2 max-w-screen-2xl mx-auto py-4 pr-6 relative h-auto">
-      <Reveal />
       <SideBar />
       <main className="w-[68%] flex flex-col">
         <div className="w-full">
