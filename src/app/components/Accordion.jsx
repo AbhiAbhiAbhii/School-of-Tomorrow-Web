4328ed2afae9a3408691e5bebee90dfe712f8138
AbhiAbhiAbhii <abhilash@gradical.xyz>
2024-07-18 12:11:06 +0530

    update

--- src/app/components/Accordion.jsx
+++ src/app/components/Accordion.jsx
@@ -1,27 +1,38 @@
 'use client'
 
+import { useEffect, useState } from "react"
+
 export default function Accordion({ data, className }) {
+
+    const colors = ['#FFD238', '#EF4976', '#FFFFFF', '#13C1E3']
+    const [randomColors, setRandomColors] = useState(['#13C1E3'])
+
+    useEffect(() => {
+
+        const generateRandomColor = () => {
+
+            return data.map(() => {
+                return colors[Math.floor(Math.random() * colors.length)]
+            })
+        }
+
+        setRandomColors(() => generateRandomColor())
+
+    }, [data])
 
 
   return (
     <ul className={`${className}`}>
-        {data.map((item) => {
-
-            let colors = ['#FFD238', '#EF4976', '#FFFFFF', '#13C1E3']
-
-            const colorsLength = colors.length
-
-            const randomBg = colors[Math.floor(Math.random() * colorsLength)]
-
+        {data.map((item, index) => {
 
             return(
                 <li    
                     key={item.id}
                     style={{
-                        background: `${randomBg}`
+                        background: `${randomColors[index]}`
                     }}
                 >
-                    Accordion item
+                    {item.title}
                 </li>
             )
         })}
