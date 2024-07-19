3ce2a1913815be73122d6fd854aa299136a8f4fc
AbhiAbhiAbhii <abhilash@gradical.xyz>
2024-07-19 09:47:18 +0530

    meta & favicon update

--- src/app/layout.js
+++ src/app/layout.js
@@ -45,11 +45,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* <link rel="alternate" hreflang="en-US" href="/en-US" />
+        <link rel="alternate" hreflang="en-US" href="/en-US" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" /> */}
+        <link rel="manifest" href="/site.webmanifest" />
         <meta property="og:image" content="/meta-img.png" />
         <meta property="twitter:image" content="/meta-img.png" />
       </head>
